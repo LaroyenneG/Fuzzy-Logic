45a52fd9761d8c0e63b8bd60c49a0a53c7c16441
laroyenneg <laroyenneg@gmail.com>
2019-01-30 20:11:15 +0100

    test runner

--- src/BinaryExpression.cpp
+++ src/BinaryExpression.cpp
@@ -1,5 +1,1 @@
-//
-// Created by guillaume on 30/01/19.
-//
-
 #include "BinaryExpression.h"
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+
+    if (argc != 1) {
+        std::cerr << "./LogiqueFloue" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+
+    CppUnit::TextTestRunner runner;
+
+    runner.run();
+
+    return EXIT_SUCCESS;
 }
