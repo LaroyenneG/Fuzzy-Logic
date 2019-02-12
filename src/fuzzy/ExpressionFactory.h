d5a9e1806af48692d3619a9df2c1440823447748
laroyenneg <laroyenneg@gmail.com>
2019-02-12 11:33:09 +0100

    cmake variable

---
+++ src/fuzzy/ExpressionFactory.h
@@ -0,0 +1,12 @@
+#ifndef LOGIQUEFLOUE_EXPRESSIONFACTORY_H
+#define LOGIQUEFLOUE_EXPRESSIONFACTORY_H
+
+namespace fuzzy {
+    template<typename T>
+    class ExpressionFactory {
+
+    };
+}
+
+
+#endif //LOGIQUEFLOUE_EXPRESSIONFACTORY_H
---
+++ src/fuzzy/FuzzyFactory.h
@@ -0,0 +1,12 @@
+#ifndef LOGIQUEFLOUE_FUZZYFACTORY_H
+#define LOGIQUEFLOUE_FUZZYFACTORY_H
+
+namespace fuzzy {
+    template<typename T>
+    class FuzzyFactory {
+
+    };
+}
+
+
+#endif //LOGIQUEFLOUE_FUZZYFACTORY_H
