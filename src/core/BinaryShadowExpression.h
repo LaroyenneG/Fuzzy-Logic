c8b8e95037d7b8b6c93f4c9cfa1495f1b8d9c426
Bonjouravous <alexis.deckerwurtz@gmail.com>
2019-02-13 14:09:30 +0100

    Ajout override a la methode evaluate BinaryShadowExpression

--- src/core/BinaryShadowExpression.h
+++ src/core/BinaryShadowExpression.h
@@ -7,14 +7,14 @@ namespace core {
     template<typename T>
     class BinaryShadowExpression : public BinaryExpression<T> {
     public:
-        T evalutate(const Expression<T> *left, const Expression<T> *right) const;
+        T evaluate(const Expression<T> *left, const Expression<T> *right) const override;
 
     private:
         T *target;
     };
 
     template<typename T>
-    T BinaryShadowExpression<T>::evalutate(const Expression<T> *left, const Expression<T> *right) const {
+    T BinaryShadowExpression<T>::evaluate(const Expression<T> *left, const Expression<T> *right) const {
         if (target != nullptr) {
             return target.evaluate(*left, *right);
         }
