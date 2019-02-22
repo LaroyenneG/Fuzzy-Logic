f05cca85edcfa93ebd58a1511243bd32d6bbc17d
laroyenneg <laroyenneg@gmail.com>
2019-02-22 18:03:36 +0100

    unit test value model

--- test/core/ValueModelTest.cpp
+++ test/core/ValueModelTest.cpp
@@ -1,10 +1,20 @@
-//
-// Created by guillaume on 22/02/19.
-//
-
 #include "ValueModelTest.h"
 
 
-void ValueModelTest::testContructor() {
+void ValueModelTest::testConstructor() {
 
+    ValueModel valueModel(3.0);
+
+    CPPUNIT_ASSERT_EQUAL(3.0, valueModel.evaluate());
+}
+
+void ValueModelTest::testSetter() {
+
+    ValueModel valueModel(5.0);
+
+    CPPUNIT_ASSERT_EQUAL(5.0, valueModel.evaluate());
+
+    valueModel.setValue(6.0);
+
+    CPPUNIT_ASSERT_EQUAL(6.0, valueModel.evaluate());
 }
--- test/core/ValueModelTest.h
+++ test/core/ValueModelTest.h
@@ -1,24 +1,23 @@
-//
-// Created by guillaume on 22/02/19.
-//
-
 #ifndef LOGIQUEFLOUE_VALUEMODELTEST_H
 #define LOGIQUEFLOUE_VALUEMODELTEST_H
 
 #include <cppunit/extensions/HelperMacros.h>
 
+#include "ValueModel.h"
+
+using namespace core;
 
 class ValueModelTest : public CppUnit::TestFixture {
 
 CPPUNIT_TEST_SUITE(ValueModelTest);
-        CPPUNIT_TEST(testContructor);
+            CPPUNIT_TEST(testConstructor);
+            CPPUNIT_TEST(testSetter);
     CPPUNIT_TEST_SUITE_END();
 
 public:
+    void testConstructor();
 
-    void testContructor();
-
-
+    void testSetter();
 };
 
 
