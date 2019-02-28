e3c8216b0c03a400594899439f0fec4272a499fc
Erwan <duroux.e@laposte.net>
2019-02-28 20:59:16 +0100

    add the NOT interface

---
+++ src/fuzzy/Not.h
@@ -0,0 +1,14 @@
+#ifndef LOGIQUEFLOUE_NOT_H
+#define LOGIQUEFLOUE_NOT_H
+
+#include "UnaryExpression.h"
+
+namespace fuzzy {
+    template<typename T>
+    class Not : public core::UnaryExpression<T> {
+    public:
+
+    };
+
+}
+#endif //LOGIQUEFLOUE_NOT_H
