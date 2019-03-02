7734adab8b07fb947d916a86a10a5a9c8783cfce
laroyenneg <laroyenneg@gmail.com>
2019-03-02 20:50:21 +0100

    pairs

--- src/fuzzy/Shape.h
+++ src/fuzzy/Shape.h
@@ -1,12 +1,36 @@
 #ifndef LOGIQUEFLOUE_SHAPE_H
 #define LOGIQUEFLOUE_SHAPE_H
 
+#include <vector>
+#include <map>
+
+#include "PointAlreadyAddedShapeException.h"
+
 namespace fuzzy {
 
     template<typename T>
     class Shape {
+    private:
+        std::pair<std::vector<T>, std::vector<T>> points;
 
+    public:
+        explicit Shape() = default;
+
+        void addPoint(const T &x, const T &y);
     };
+
+    template<typename T>
+    void Shape<T>::addPoint(const T &x, const T &y) {
+
+        for (int i = 0; i < points.first.size(); ++i) {
+            if (points.first[i] == x && points.second[i] == y) {
+                throw exception::PointAlreadyAddedShapeException();
+            }
+        }
+
+        points.first.push_back(x);
+        points.second.push_back(y);
+    }
 }
 
 
