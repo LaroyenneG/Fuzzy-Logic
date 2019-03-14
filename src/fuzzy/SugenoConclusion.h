9480a1a865745108df0367b0e06d8123a8a08033
Bonjouravous <alexis.deckerwurtz@gmail.com>
2019-03-14 09:49:25 +0100

    Add class SugenoDefuzz and SugenoConclusion

---
+++ src/fuzzy/SugenoConclusion.h
@@ -0,0 +1,26 @@
+#ifndef LOGIQUEFLOUE_SUGENOCONCLUSION_H
+#define LOGIQUEFLOUE_SUGENOCONCLUSION_H
+
+#include <vector>
+#include "NaryExpression.h"
+
+namespace fuzzy {
+    template<typename T>
+
+    class SugenoConclusion : core::NaryExpression<T> {
+
+    private:
+        std::vector<T> coef;
+
+    public:
+        T evaluate(const core::Expression<T> **operands) const override;
+    };
+
+    template<typename T>
+
+    T SugenoConclusion<T>::evaluate(const core::Expression<T> **operands) const {
+        return nullptr;
+    }
+}
+
+#endif //LOGIQUEFLOUE_SUGENOCONCLUSION_H
---
+++ src/fuzzy/SugenoDefuzz.h
@@ -0,0 +1,23 @@
+#ifndef LOGIQUEFLOUE_SUGENODEFUZZ_H
+#define LOGIQUEFLOUE_SUGENODEFUZZ_H
+
+#include "NaryExpression.h"
+
+namespace fuzzy {
+    template<typename T>
+
+    class SugenoDefuzz : core::NaryExpression<T> {
+
+    public:
+        T evaluate(const core::Expression<T> **oprands) const override;
+
+    };
+
+    template<typename T>
+
+    T SugenoDefuzz<T>::evaluate(const core::Expression<T> **oprands) const {
+        return nullptr;
+    }
+}
+
+#endif //LOGIQUEFLOUE_SUGENODEFUZZ_H
