2136f9c5d0407aa680f1da9a44360ceb5749589b
LaroyenneG <laroyenneg@gmail.com>
2019-03-16 00:27:41 +0100

    qt library

---
+++ titanic/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+    if (argc != 1) {
+        std::cerr << "Titanic" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
