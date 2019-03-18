53c1fe12473ec97267148bf097a0c40e642b1048
laroyenneg <laroyenneg@gmail.com>
2019-03-18 22:54:33 +0100

    propeller

--- titanic/model/Titanic.cpp
+++ titanic/model/Titanic.cpp
@@ -1,4 +1,15 @@
 
+#include "Titanic.h"
+
 namespace model {
 
+    Titanic::Titanic(const std::set<std::pair<double, double >> &points, double _course, double _rudder,
+                     double _propellerAccelerator, double _enginePower)
+            : Object2D(points),
+              course(_course),
+              rudder(_rudder),
+              propellerSpeed(_propellerAccelerator),
+              propellerAccelerator(_enginePower) {
+
+    }
 }
--- titanic/model/Titanic.h
+++ titanic/model/Titanic.h
@@ -6,12 +6,24 @@
 
 #include "Object2D.h"
 
+#define DEFAULT_COURSE 0.0
+#define DEFAULT_RUDDER 0.0
+#define DEFAULT_PROPERLER 0.0
+#define DEFAULT_ENGINE_POWER 0.0
+
 namespace model {
 
     class Titanic : public Object2D {
 
+    private:
+        double course;          // radian
+        double rudder;          // radian
+        double propellerSpeed;      // radian / s
+        double propellerAccelerator;    // radian / s²
+
     public:
-        Titanic() : Object2D(std::set<std::pair<double, double >>()) {}
+        explicit Titanic(const std::set<std::pair<double, double >> &points, double _course, double _rudder,
+                         double _propellerAccelerator, double _enginePower);
     };
 }
 
