51766ae4aa10666617d152eb13efa1c8b248849f
LaroyenneG <laroyenneg@gmail.com>
2019-04-09 13:14:09 +0200

    fuzzy logic prepare refactor

--- src/fuzzylogic/fuzzylogic.h
+++ src/fuzzylogic/fuzzylogic.h
@@ -1,9 +1,42 @@
 #ifndef LOGIQUEFLOUE_FUZZYLOGIC_H
 #define LOGIQUEFLOUE_FUZZYLOGIC_H
 
+#include "NaryShadowExpression.h"
+#include "UnaryShadowExpression.h"
+#include "BinaryShadowExpression.h"
+#include "NaryExpressionModel.h"
+#include "BinaryExpressionModel.h"
+#include "UnaryExpressionModel.h"
+#include "ValueModel.h"
 #include "Expression.h"
 
 namespace fuzzylogic {
+
+
+    /****************************************** core *************************************************/
+
+    /* Model */
+
+    typedef core::ValueModel<double> ValueModel;
+
+    typedef core::UnaryExpressionModel<double> UnaryExpressionModel;
+
+    typedef core::BinaryExpressionModel<double> BinaryExpressionModel;
+
+    typedef core::NaryExpressionModel<double> NaryExpressionModel;
+
+
+    /* Shadow */
+
+    typedef core::UnaryShadowExpression<double> UnaryShadowExpression;
+
+    typedef core::BinaryShadowExpression<double> BinaryShadowExpression;
+
+    typedef core::NaryShadowExpression<double> NaryShadowExpression;
+
+
+    /***************************************** fuzzy **************************************************/
+
 
 }
 
